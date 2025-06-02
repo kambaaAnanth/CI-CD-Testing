ee73accc501e98029925c9fb17c11bb0cdbbcbc4
Ananth <50041491+kambaaAnanth@users.noreply.github.com>
2025-06-02 10:04:51 +0530

    Update DriverFactory.java

--- src/main/java/com/facebook/app/utils/DriverFactory.java
+++ src/main/java/com/facebook/app/utils/DriverFactory.java
@@ -13,6 +13,13 @@ public class DriverFactory {
             ChromeOptions options = new ChromeOptions();
             options.addArguments("--no-sandbox");
             options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--disable-dev-shm-usage"); // Overcome limited resource problems
+        // Options below are often recommended for CI environments
+        options.addArguments("--window-size=1920,1080"); // Set a default window size
+        options.addArguments("--disable-gpu"); // Often recommended for headless
+        options.addArguments("--disable-extensions"); // Disable extensions
+        options.addArguments("--disable-setuid-sandbox"); // Another sandbox related argument
+        options.addArguments("--remote-debugging-port=9222");
             driver.set(new ChromeDriver(options));
         }
         return driver.get();
