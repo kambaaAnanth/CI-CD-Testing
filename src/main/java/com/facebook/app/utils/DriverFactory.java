7d0bdbf0391bcafe06ea3509255315012b705560
Ananth <50041491+kambaaAnanth@users.noreply.github.com>
2025-06-02 10:10:25 +0530

    Update DriverFactory.java

--- src/main/java/com/facebook/app/utils/DriverFactory.java
+++ src/main/java/com/facebook/app/utils/DriverFactory.java
@@ -11,6 +11,7 @@ public class DriverFactory {
         if (driver.get() == null) {
             WebDriverManager.chromedriver().setup();
             ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless");
             options.addArguments("--no-sandbox");
             options.addArguments("--disable-dev-shm-usage");
             options.addArguments("--disable-dev-shm-usage"); // Overcome limited resource problems
